7d2a28aa2c082aeef3b94d83c10081dfd76e12ea
KMHKS <Trigghanks@gmail.com>
2022-10-23 18:09:51 +0100

    updated api: description, humidity, wind and temp

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
-function displayTemperature (response); {
-  let temperatureElement = document.querySelector()
-  let cityElement = document.querySelector();
-  temperatureElement.innerHTML = Math.round
-  (response.data.main.temp);
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
-
-
-let apiKey = "d58f3146b0a0fc1ocbt58fad1a6c4e24";
-let apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${apiKey}&units=metric";
+let apiKey = "ff1d9ea9376b5c27a82e04fc2b2abdbb";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
