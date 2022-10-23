767b36b1977d101648ef1109f74c3c6b74be266f
KMHKS <Trigghanks@gmail.com>
2022-10-23 08:50:30 +0100

    starting structure of api

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
+function displayTemperature (response); {
+  let temperatureElement = document.querySelector()
+  let cityElement = document.querySelector();
+  temperatureElement.innerHTML = Math.round
+  (response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+}
+
+
+
 let apiKey = "d58f3146b0a0fc1ocbt58fad1a6c4e24";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/forecast?lon={lon}&lat=latitude&key={key}=metric";
+let apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${apiKey}&units=metric";
+
+axios.get(apiUrl).then(displayTemperature);
